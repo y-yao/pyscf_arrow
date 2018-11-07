a036ce66236f8d2cc91da10c0ad839d9a4fa6967
Yuan Yao <andrewyaoinzz@gmail.com>
2018-11-06 20:04:41 -0500

    fix_orb_sym

--- pyscf/shciscf/settings.py
+++ pyscf/shciscf/settings.py
@@ -6,11 +6,11 @@
 
 import os
 
+PYSCF_HOME = '/home/yuanyao/pyscf_my_csv/'
 SHCIEXE = '/home/yuanyao/SHCI/shci/shci_64'
 SHCIRUNTIMEDIR = '.'
 MPIPREFIX = '' 
-SHCILIB = '/home/yuanyao/pyscf_my_csv/pyscf/future/shciscf/SHCI_tools.so'
-#SHCISCFPATH = '/home/yuanyao/pyscf_my_csv/pyscf/future/shciscf'
+SHCILIB = PYSCF_HOME + 'pyscf/future/shciscf/SHCI_tools.so'
 
-LZ_RELABEL = '/home/yuanyao/pyscf_my_csv/pyscf/shciscf/Lz_relabel_f90/fix_orb_sym_all'
+LZ_RELABEL = PYSCF_HOME + 'pyscf/shciscf/Lz_relabel_f90/fix_orb_sym_all'
 
